dfc47d168cec634d1ba2695fd5873d0b0137826c
markhinojosa1212@gmail.com <markhinojosa1212@gmail.com>
2018-05-20 12:03:48 -0500

    added static serve code to api

--- dist/index.js
+++ dist/index.js
@@ -20,6 +20,12 @@ app.get("/test", function (req, res, next) {
 // Serve static files from the React app
 app.use(_express2.default.static(path.join(__dirname, "client/build")));
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log("Listening on port :" + port);
--- server/index.js
+++ server/index.js
@@ -11,6 +11,12 @@ app.get("/test", function(req, res, next) {
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Listening on port :${port}`);
